9e6a2379bd15a687596c94f18bf66bdd4d533a3d
alex <eafo72@gmail.com>
2025-03-26 20:06:03 -0600

    cambio de servidor

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const clienteAxios = axios.create({
- baseURL: "https://api.mellfashionboutique.com"
+ baseURL: "https://apinew.mellfashionboutique.com"
  //baseURL: "http://localhost:4000"
 })
 
